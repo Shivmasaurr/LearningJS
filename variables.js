b170efb86bc9db62aeb2534658d20f59c6f7fecc
Shivam Upadhyay <122978849+Shivmasaurr@users.noreply.github.com>
2024-02-11 10:43:40 +0530

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,9 @@
+const accountID= 1234
+let accountEmail = "workonlyshivam@gmail.com"
+var accountpw= 1111
+accountcity= "Delhi"
+
+console.table([accountID, accountEmail, accountpw, accountcity])
+
+
+
